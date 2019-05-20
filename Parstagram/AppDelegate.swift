91f7605cd37df1805bb4d1fd16728a8446d0c524
Pingde Gu <peterouwen@gmail.om>
2019-05-19 19:13:08 -0700

    Part 2

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -23,6 +23,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://polar-tor-87474.herokuapp.com/parse"
             })
         )
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
+        
         return true
     }
 
